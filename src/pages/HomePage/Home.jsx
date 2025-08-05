810464fd70452e57f6da648f4af84889afae0f03
Robiul Hasan Raian <99517741+NaimaNITU@users.noreply.github.com>
2025-08-06 00:43:25 +0600

    ..

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -5,9 +5,8 @@ import EmployeeSection from "../../components/HomePageComponents/EmployeeSection
 import A2ITGallery from "../../components/HomePageComponents/A2ITGallery";
 import StatCard from "../../components/HomePageComponents/StatCard";
 import WhoRWe from "../../components/HomePageComponents/WhoRWe";
-import WhatWeOffer from "../../components/HomePageComponents/WHatWeOffer";
-import Footer from "../../components/Footer/Footer";
 import CompanyGallery from "../../components/HomePageComponents/CompanyGallery";
+import WhatWeOffer from "./../../components/HomePageComponents/WhatWeOffer";
 
 const Home = () => {
   return (
